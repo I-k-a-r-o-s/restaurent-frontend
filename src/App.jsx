ab0185830986a5c4592936a4ddc758a5dea0e948
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-01-09 17:40:34 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import BookTable from "./pages/BookTable";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Menu from "./pages/Menu";
+import MyBookings from "./pages/MyBookings";
+import MyOrders from "./pages/MyOrders";
+import Signup from "./pages/Signup";
+import MenuDetails from "./pages/MenuDetails.jsx";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/book-table" element={<BookTable />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/menu-details/:id" element={<MenuDetails />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/my-orders" element={<MyOrders />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
+  );
+};
+export default App;
